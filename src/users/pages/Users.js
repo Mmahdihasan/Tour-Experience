08c3215d7ad99c7348fe8937be5459e1be12f3bc
Mmahdihasan <mahdistu01999@gmail.com>
2024-08-28 16:49:58 +0600

    useritem

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import UserList from '../components/UserList';
 
 const Users = () => {
-    return <h1>User Works</h1>
-
+  const USERS = [
+    // {
+    //   id: 'u1',
+    //   name: 'wrath',
+    //   image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/220px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg',
+    //   places: 3,
+    // },
+  ];
+  return <UserList items={USERS}/>;
 };
 
 export default Users;
