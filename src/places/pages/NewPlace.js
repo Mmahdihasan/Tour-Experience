5afc73305eb159db2d2ae4a7c11b8dd1b9af5b8a
Mmahdihasan <mahdistu01999@gmail.com>
2024-09-05 01:10:32 +0600

    valid

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,13 @@ import './NewPlace.css';
 const NewPlaces = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="please enter a valid title."
+      />
     </form>
   );
 };
